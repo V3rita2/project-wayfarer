ce4ce6f273328bab77cb85b84c5cbe0953f8aa97
rose belmont <rose.daemon.belmont@gmail.com>
2021-10-13 16:12:29 -0700

    resolves #35

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+# model for the city pages, will be hardcoded
+class City(models.Model):
+
+    name = models.CharField(max_length=256)
+    # Will also need a route to upload to
+    image = models.ImageField(upload_to=None)
+    description = models.TextField
+
 #model for parks
 class Park(models.Model):
 
@@ -11,3 +19,4 @@ class Park(models.Model):
     #no character limit to description, front end will cut off 1000 or more unless clicked on
     description = models.TextField
     created_at = models.DateTimeField(auto_now_add=True)
+    city = models.ForeignKey(City)
