fc859f57c8d7488d779e8f2a162ae9af74c88bd6
rose belmont <rose.daemon.belmont@gmail.com>
2021-10-13 16:00:57 -0700

    resolves  #32

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#model for parks
+class Park(models.Model):
+
+    name = models.CharField(max_length=256)
+    #upload_to will have to have a route dedicated to it, possibly have a few for posting.
+    image = models.ImageField(upload_to=None)
+    #no character limit to description, front end will cut off 1000 or more unless clicked on
+    description = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
