0538dbef52cbffad898b08b88595acb89e0b28da
MaricelaDuron0809 <emtdavidian@gmail.com>
2021-10-12 23:37:24 -0700

    add home route to views.py

--- pursuit_of_parks/main_app/views.py
+++ pursuit_of_parks/main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
