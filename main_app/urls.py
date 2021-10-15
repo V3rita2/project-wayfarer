aeca58bed3059256f07e44f93dc2471870f969cc
Matt DeZarn <matt.dezarn@gmail.com>
2021-10-15 09:33:58 -0700

    fixed typo in urls and views. Started base of home page.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     
     #path for single post about a park page
     #need to come back to update route
-    path('park/', views.Post.as_view(), name="post"),
+    path('posts/', views.Post.as_view(), name="posts"),
     #path to login- might need to update route later
     path('login/', views.Login.as_view(), name="login"),
     #path for user to sign-up/register
